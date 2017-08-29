7fb726a28232662757e87c675564dcc138a4566a
swap22 <sharma.sswapnil@gmail.com>
2017-08-29 22:44:15 +0530

    created menu and other folder

--- index.php
+++ index.php
@@ -18,8 +18,31 @@
                     Total Shopping Cart Price: 500$ Total Items : 5
                     </a>
                 </div><!-- col-md-6 offer Starts -->
-            </div><!-- container Starts -->
-        </div><!-- top Starts -->
+                <div class="col-md-6"><!-- col-md-6 Starts -->
+                    <ul class="menu"><!-- menu Starts -->
+                        <li>
+                            <a href="customer_register.php">
+                                Register
+                            </a>
+                        </li>
+                            <a href="customer_register.php">
+                                My Account
+                            </a>
+                        <li>
+                        </li>
+                            <a href="cart.php">
+                            Go to Cart
+                            </a>
+                        <li>
+                            <a href="customer_register.php">
+                            Login
+                            </a>
+                       </li>
+                    </ul><!-- menu Ends -->
+                </div><!-- col-md-6 Ends -->
+            </div><!-- container end -->
+        </div><!-- top end -->
+        
 
         <script src="js/jquery.min.js"> </script>
         <script src="js/bootstrap.min.js"></script>
