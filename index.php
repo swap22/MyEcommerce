27abe0bac528a3227454ebdd8657c5cda7a5ac46
swap22 <sharma.sswapnil@gmail.com>
2017-08-29 12:42:42 +0530

    stylesheet and js file linked and dumy text created

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MY Ecommerce Store!!!! </title>
+        <link href="styles/bootstrap.min.css" rel="stylesheet">
+        <link href="styles/style.css" rel="stylesheet">
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <div id="top"><!-- top Starts -->
+            <div class="container"><!-- container Starts -->
+                <div class="col-md-6 offer"><!-- col-md-6 offer Starts -->
+                    <a href="#" class="btn btn-success btn-sm" >
+                    Welcome Swapnil
+                    </a>
+                    <a href="#">
+                    Total Shopping Cart Price: 500$ Total Items : 5
+                    </a>
+                </div><!-- col-md-6 offer Starts -->
+            </div><!-- container Starts -->
+        </div><!-- top Starts -->
+
+        <script src="js/jquery.min.js"> </script>
+        <script src="js/bootstrap.min.js"></script>
+     </body>
+</html>
