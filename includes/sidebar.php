b5f9e1bd74825f3f4f26fa550635dd2adbba360e
swap22 <sharma.sswapnil@gmail.com>
2017-09-05 17:35:29 +0530

    sidebar for shop page for filter section

---
+++ includes/sidebar.php
@@ -0,0 +1,28 @@
+<div class="panel panel-default sidebar-menu"><!--- panel panel-default sidebar-menu Starts -->
+    <div class="panel-heading"><!-- panel-heading Starts -->
+        <h3 class="panel-title"> Products Categories </h3>
+    </div><!-- panel-heading Ends -->
+    <div class="panel-body"><!-- panel-body Starts -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Starts -->
+            <li><a href="shop.php">T-shirt</a></li>
+            <li><a href="shop.php">Jacket</a></li>
+            <li><a href="shop.php">Blazzer</a></li>
+            <li><a href="shop.php">Jeans</a></li>
+            <li><a href="shop.php">Formals</a></li>     
+        </ul><!-- nav nav-pills nav-stacked category-menu Ends -->
+    </div><!-- panel-body Ends -->
+</div><!--- panel panel-default sidebar-menu Ends -->
+
+<div class="panel panel-default sidebar-menu"><!--- panel panel-default sidebar-menu Starts -->
+    <div class="panel-heading"><!-- panel-heading Starts -->
+        <h3 class="panel-title">Categories </h3>
+    </div><!-- panel-heading Ends -->
+    <div class="panel-body"><!-- panel-body Starts -->
+        <ul class="nav nav-pills nav-stacked category-menu"><!-- nav nav-pills nav-stacked category-menu Starts -->
+            <li><a href="shop.php">Mens</a></li>
+            <li><a href="shop.php">Women</a></li>
+            <li><a href="shop.php">kids</a></li>
+            <li><a href="shop.php">Others</a></li>     
+        </ul><!-- nav nav-pills nav-stacked category-menu Ends -->
+    </div><!-- panel-body Ends -->
+</div><!--- panel panel-default sidebar-menu Ends -->
