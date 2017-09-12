339da477bf117f9045cfa58776204dc4e805abac
swap22 <sharma.sswapnil@gmail.com>
2017-09-12 09:09:30 +0530

    order page is created for payment

---
+++ customer/my_orders.php
@@ -0,0 +1,65 @@
+<center><!-- center Starts -->
+    <h1>My Orders</h1>
+        <p class="lead"> Your orders on one place.</p>
+        <p class="text-muted" >
+            If you have any questions, please feel free to <a href="../contact.php" > contact us,</a> our customer service center is working for you 24/7.
+        </p>
+</center><!-- center Ends -->
+<hr>
+<div class="table-responsive" ><!-- table-responsive Starts -->
+    <table class="table table-bordered table-hover" ><!-- table table-bordered table-hover Starts -->
+        <thead><!-- thead Starts -->
+        <tr>
+            <th>O N:</th>
+            <th>Due Amount:</th>
+            <th>Invoice No:</th>
+            <th>Qty:</th>
+            <th>Size:</th>
+            <th>Order Date:</th>
+            <th>Paid/Unpaid:</th>
+            <th>Status:</th>
+        </tr>
+        </thead><!-- thead Ends -->
+        <tbody><!-- tbody Starts -->
+            <tr><!-- tr Starts -->
+                <th>#1</th>
+                <td>$80</td>
+                <td>65685</td>
+                <td>2</td>
+                <td>Small</td>
+                <td>2017-8-15</td>
+                <td>Unpaid</td>
+                <td>
+                <a href="confirm.php" target="blank" class="btn btn-primary btn-sm" > Confirm If Paid </a>
+                </td>
+            </tr><!-- tr Ends -->
+             <tr><!-- tr Starts -->
+                <th>#2</th>
+                <td>$200</td>
+                <td>652585</td>
+                <td>4</td>
+                <td>Large</td>
+                <td>2017-8-15</td>
+                <td>Unpaid</td>
+                <td>
+                <a href="confirm.php" target="blank" class="btn btn-primary btn-sm" > Confirm If Paid </a>
+                </td>
+            </tr><!-- tr Ends -->
+             <tr><!-- tr Starts -->
+                <th>#3</th>
+                <td>$80</td>
+                <td>65685</td>
+                <td>2</td>
+                <td>Small</td>
+                <td>2017-8-15</td>
+                <td>Unpaid</td>
+                <td>
+                <a href="confirm.php" target="blank" class="btn btn-primary btn-sm" > Confirm If Paid </a>
+                </td>
+            </tr><!-- tr Ends -->
+        </tbody><!-- tbody Ends -->
+    </table><!-- table table-bordered table-hover Ends -->
+</div><!-- table-responsive Ends -->
+
+
+
