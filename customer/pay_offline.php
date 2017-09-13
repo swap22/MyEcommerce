268f703ab558fe7c2b4a68754e1f09301a2d7660
swap22 <sharma.sswapnil@gmail.com>
2017-09-13 10:00:59 +0530

    offline payment option created

---
+++ customer/pay_offline.php
@@ -0,0 +1,25 @@
+<center><!-- center Starts -->
+    <h1> Pay OffLine Using Method  </h1>
+        <p class="text-muted" >
+            If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+        </p>
+</center><!-- center Ends -->
+<hr>
+<div class="table-responsive" ><!-- table-responsive Starts -->
+    <table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+        <thead><!-- thead Starts -->
+            <tr>
+                <th> Bank Account Details </th>
+                <th> Check, Demand Draft,Cash Details: </th>
+                <th> Bank Account Details: </th>
+            </tr>
+        </thead><!-- thead Ends -->
+        <tbody><!-- tbody Starts -->
+            <tr>
+                <td> Bank Name:ubl Account No:035433 Branch Code:0342 Branch Name:Sharma bank pune	 </td>
+                <td> Mobile No:05343134543, Name:S.R.Sharma </td>
+                <td> Detail of company</td>
+            </tr>
+        </tbody><!-- tbody Ends -->
+    </table><!-- table table-bordered table-hover table-striped Ends -->
+</div><!-- table-responsive Ends -->
