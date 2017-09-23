aac916a556a504a09d24d118e83652a9d89bd842
swap22 <sharma.sswapnil@gmail.com>
2017-09-23 19:39:11 +0530

    product detail page made dynamic and product suggestion made dynamic

--- details.php
+++ details.php
@@ -3,6 +3,27 @@ include("includes/db.php");
 
 include("functions/functions.php");
 ?>
+
+<?php
+   if(isset($_GET['pro_id'])){
+        $product_id = $_GET['pro_id'];
+        $get_product = "select * from products where product_id='$product_id'";
+        $run_product = mysqli_query($con,$get_product);
+        $row_product = mysqli_fetch_array($run_product);
+        $p_cat_id = $row_product['p_cat_id'];
+        $pro_title = $row_product['product_title'];
+        $pro_price = $row_product['product_price'];
+        $pro_desc = $row_product['product_desc'];
+        $pro_img1 = $row_product['product_img1'];
+        $pro_img2 = $row_product['product_img2'];
+        $pro_img3 = $row_product['product_img3'];
+        $get_p_cat = "select * from product_categories where p_cat_id='$p_cat_id'";
+        $run_p_cat = mysqli_query($con,$get_p_cat);
+        $row_p_cat = mysqli_fetch_array($run_p_cat);
+        $p_cat_title = $row_p_cat['p_cat_title'];
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -119,6 +140,10 @@ include("functions/functions.php");
                         <a href="index.php">Home</a>
                     </li>
                     <li>Shop</li>
+                    <li> <!-- to write categories -->
+                        <a href="shop.php?p_cat=<?php echo $p_cat_id; ?>"> <?php echo $p_cat_title; ?> </a>
+                    </li>
+                    <li> <?php echo $pro_title; ?> </li>
                 </ul><!-- breadcrumb Ends -->
             </div><!--- col-md-12 Ends -->
             <div class="col-md-3"><!-- col-md-3 Starts -->
@@ -140,17 +165,17 @@ include("functions/functions.php");
                                 <div class="carousel-inner"><!-- carousel-inner Starts -->
                                     <div class="item active">
                                         <center>
-                                            <img src="admin_area/product_images/product.jpg" class="img-responsive">
+                                            <img src="admin_area/product_images/<?php echo $pro_img1; ?>" class="img-responsive">
                                         </center>
                                     </div>
                                     <div class="item">
                                         <center>
-                                            <img src="admin_area/product_images/product2.jpg" class="img-responsive">
+                                            <img src="admin_area/product_images/<?php echo $pro_img2; ?>" class="img-responsive">
                                         </center>
                                     </div>
                                     <div class="item">
                                         <center>
-                                            <img src="admin_area/product_images/product3.jpg" class="img-responsive">
+                                            <img src="admin_area/product_images/<?php echo $pro_img3; ?>" class="img-responsive">
                                         </center>
                                     </div>
                                 </div><!-- carousel-inner Ends -->
@@ -167,8 +192,9 @@ include("functions/functions.php");
                     </div><!-- col-sm-6 Ends -->
                     <div class="col-sm-6" ><!-- col-sm-6 Starts -->
                         <div class="box" ><!-- box Starts -->
-                            <h1 class="text-center" > T-Shirt Black</h1>
-                            <form action="details.php" method="post" class="form-horizontal" ><!-- form-horizontal Starts -->
+                            <h1 class="text-center" >  <?php echo $pro_title; ?> </h1>
+                            <?php add_cart(); ?>
+                            <form action="details.php?add_cart=<?php echo $product_id; ?>" method="post" class="form-horizontal" ><!-- form-horizontal Starts -->
                             <div class="form-group"><!-- form-group Starts -->
                                 <label class="col-md-5 control-label" >Product Quantity </label>
                                 <div class="col-md-7" ><!-- col-md-7 Starts -->
@@ -194,7 +220,7 @@ include("functions/functions.php");
                                     </select>
                                 </div><!-- col-md-7 Ends -->
                             </div><!-- form-group Ends -->
-                            <p class="price" > $500 </p>
+                            <p class="price" > $<?php echo $pro_price; ?> </p>
                             <p class="text-center buttons" ><!-- text-center buttons Starts -->
                             <button class="btn btn-primary" type="submit" >
                                 <i class="fa fa-shopping-cart" ></i> Add to Cart
@@ -205,17 +231,17 @@ include("functions/functions.php");
                         <div class="row" id="thumbs" ><!-- row Starts -->
                             <div class="col-xs-4" ><!-- col-xs-4 Starts -->
                                 <a href="#" class="thumb" >
-                                    <img src="admin_area/product_images/product.jpg" class="img-responsive" >
+                                    <img src="admin_area/product_images/<?php echo $pro_img1; ?>" class="img-responsive" >
                                 </a>
                             </div><!-- col-xs-4 Ends -->
                             <div class="col-xs-4" ><!-- col-xs-4 Starts -->
                                 <a href="#" class="thumb" >
-                                    <img src="admin_area/product_images/product2.jpg" class="img-responsive" >
+                                    <img src="admin_area/product_images/<?php echo $pro_img2; ?>" class="img-responsive" >
                                 </a>
                             </div><!-- col-xs-4 Ends -->
                             <div class="col-xs-4" ><!-- col-xs-4 Starts -->
                                 <a href="#" class="thumb" >
-                                    <img src="admin_area/product_images/product3.jpg" class="img-responsive" >
+                                    <img src="admin_area/product_images/<?php echo $pro_img3; ?>" class="img-responsive" >
                                 </a>
                             </div><!-- col-xs-4 Ends -->
                          </div><!-- row Ends -->
@@ -225,7 +251,7 @@ include("functions/functions.php");
                     <p><!-- p Starts -->
                     <h4>Product details</h4>
                     <p>
-                    An efficient monitoring and management of road traffic involves detection of vehicle license plate which is the unique identification mark of each vehicle. License Plate Detection involves  use of an efficient edge detection, character segmentation and recognition algorithm 
+                    <?php echo $pro_desc; ?>
                     </p>
                     <h4> Size </h4>
                     <ul>
@@ -242,39 +268,29 @@ include("functions/functions.php");
                             <h3 class="text-center"> You also like these Products </h3>
                         </div><!-- box same-height headline Ends -->
                     </div><!-- col-md-3 col-sm-6 Ends -->
-                    <div class='center-responsive col-md-3 col-sm-6' >
-                        <div class='product same-height' >
-                            <a href='details.php' >
-                                <img src='admin_area/product_images/product.jpg' class='img-responsive' >  
+                    <?php
+                        $get_products = "select * from products order by rand() LIMIT 0,3";
+                        $run_products = mysqli_query($con,$get_products); 
+                        while($row_products = mysqli_fetch_array($run_products)) {
+                            $pro_id = $row_products['product_id'];
+                            $pro_title = $row_products['product_title'];
+                            $pro_price = $row_products['product_price'];
+                            $pro_img1 = $row_products['product_img1'];
+                            echo "
+                            <div class='center-responsive col-md-3 col-sm-6' >
+                            <div class='product same-height' >
+                            <a href='details.php?pro_id=$pro_id' >
+                            <img src='admin_area/product_images/$pro_img1' class='img-responsive' >  
                             </a>
                             <div class='text' >
-                                <h3> <a href='details.php' >TShirt - Black</a> </h3>
-                                <p class='price' >$500</p>
+                            <h3> <a href='details.php?pro_id=$pro_id' >$pro_title</a> </h3>
+                            <p class='price' >$$pro_price</p>
                             </div>
-                        </div>
-                    </div>
-                    <div class='center-responsive col-md-3 col-sm-6' >
-                        <div class='product same-height' >
-                            <a href='details.php' >
-                                <img src='admin_area/product_images/product2.jpg' class='img-responsive' >  
-                            </a>
-                            <div class='text' >
-                                <h3> <a href='details.php' >TShirt - Black</a> </h3>
-                                <p class='price' >$500</p>
                             </div>
-                        </div>
-                    </div>
-                    <div class='center-responsive col-md-3 col-sm-6' >
-                        <div class='product same-height' >
-                            <a href='details.php' >
-                                <img src='admin_area/product_images/product3.jpg' class='img-responsive' >  
-                            </a>
-                            <div class='text' >
-                                <h3> <a href='details.php' >TShirt - Black</a> </h3>
-                                <p class='price' >$500</p>
                             </div>
-                        </div>
-                    </div>
+                            ";
+                        }
+                    ?>
                 </div><!-- row same-height-row Ends -->
             </div><!-- col-md-9 Ends -->
         </div><!-- container Ends -->
