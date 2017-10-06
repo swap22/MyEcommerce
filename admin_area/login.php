a30aa7ef661ea850bcc5ee572e45b1a8eaa82f76
swap22 <sharma.sswapnil@gmail.com>
2017-10-07 04:48:22 +0530

    linking the database with the log in page

--- admin_area/login.php
+++ admin_area/login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include("includes/db.php");
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -18,3 +22,20 @@
         </div><!-- container Ends -->
     </body>
 </html>
+<?php
+    if(isset($_POST['admin_login'])){
+        $admin_email = mysqli_real_escape_string($con,$_POST['admin_email']);
+        $admin_pass = mysqli_real_escape_string($con,$_POST['admin_pass']);
+        $get_admin = "select * from admins where admin_email='$admin_email' AND admin_pass='$admin_pass'";
+        $run_admin = mysqli_query($con,$get_admin);
+        $count = mysqli_num_rows($run_admin);
+        if($count==1){
+            $_SESSION['admin_email']=$admin_email;
+            echo "<script>alert('You are Logged in into admin panel')</script>";
+            echo "<script>window.open('index.php?dashboard','_self')</script>";
+        }
+        else {
+            echo "<script>alert('Email or Password is Wrong')</script>";
+        }
+    }
+?>
