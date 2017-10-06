21326d70b2498cd673b2799c8e138881ccc70922
swap22 <sharma.sswapnil@gmail.com>
2017-10-06 23:47:18 +0530

    added login for admin

---
+++ admin_area/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Admin Login</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" >
+        <link rel="stylesheet" href="css/login.css" >
+    </head>
+    <body>
+        <div class="container" ><!-- container Starts -->
+        <form class="form-login" action="" method="post" ><!-- form-login Starts -->
+        <h2 class="form-login-heading" >Admin Login</h2>
+        <input type="text" class="form-control" name="admin_email" placeholder="Email Address" required >
+        <input type="password" class="form-control" name="admin_pass" placeholder="Password" required >
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="admin_login" >
+        Log in
+        </button>
+        </form><!-- form-login Ends -->
+        </div><!-- container Ends -->
+    </body>
+</html>
